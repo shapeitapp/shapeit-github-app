19ec97de94c241e6c847856e9329415a6b687e0c
Samir AMZANI <samir.amzani@gmail.com>
2023-11-04 16:30:05 +0100

    feat: support project kind trigger

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const { handleNewPitch } = require('./lib/onboarding')
+const { handleNewPitch, getProjectNodeItem } = require('./lib/onboarding')
 const { parseIssueDescription, addScopeToBet } = require('./lib/scopes')
+
+const ITEM_ISSUE_TYPE = "Issue"
+const KIND_FIELD = "Kind"
+const PITCH_KIND_LABEL = "Pitch"
+const BET_KIND_LABEL = "Bet"
 
 module.exports = (app) => {
   app.log.info("Yay, the app was loaded!")
@@ -19,6 +24,27 @@ module.exports = (app) => {
   })
 
   app.on("projects_v2_item.edited", async (context) => {
-    console.log(context.payload)
+    console.log(`projects_v2_item.edited triggered`)
+
+    const projectNodeId = context.payload.projects_v2_item.project_node_id
+    const itemType = context.payload.projects_v2_item.content_type
+    const itemNodeId = context.payload.projects_v2_item.node_id
+    if (itemType === "Issue") {
+      const item = await getProjectNodeItem(context, projectNodeId, itemNodeId)
+      const pitchType = item.fieldValues.nodes.find(item => item?.field?.name === KIND_FIELD && item?.name === PITCH_KIND_LABEL)
+      const betType = item.fieldValues.nodes.find(item => item?.field?.name === KIND_FIELD && item?.name === BET_KIND_LABEL)
+      const owner = item.content.repository.owner.login
+      const repository = item.content.repository.name
+      const issueNodeId = item.content.id
+      const issueNumber = item.content.number
+      console.log(owner, repository, issueNodeId)
+      if (pitchType || betType) {
+        console.log("That's a pitch or a Bet")
+        await handleNewPitch(context, owner, repository, issueNodeId, issueNumber)
+      }
+      console.log(`item`, item)
+    } else {
+      console.log("Not an issue")
+    }
   })
 };
